103966d4b47d3e5193acb56c5b6ac98f8aca6ae3
Antonio José Masiá <dev@ajmasia.me>
2023-09-27 21:45:28 +0200

    add gitsigns plugin

---
+++ lua/user/plugins/gitsigns.lua
@@ -0,0 +1,9 @@
+return {
+	"lewis6991/gitsigns.nvim",
+	-- if plugin has a setup function we can load it automagically using config = true
+	opts = {
+		numhl = true,                     -- toggle with :Gitsigns toggle_numhl
+		max_file_length = 10000           -- disable if file is longer than this (in lines)
+	},
+}
+
